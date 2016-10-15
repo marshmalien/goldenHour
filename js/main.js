eff93f2a1c0e209f82ce220ff4367b19726859df
Marliana Lara <marliana.lara@gmail.com>
2016-10-15 14:15:10 -0400

    input field and state dropdown returns sun phase times

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 $(document).ready(function() {
 
-  $('input').on('keypress', function(e) {
-    var code = e.keyCode || e.which;
-    if(code == 13) {
+  $('select').on('change', function() {
     var city= $('input').val();
     user.city = city;
-    }
-  });
-
-  $('select').on('change', function() {
     var state= $(this)[0].value;
     user.state = state;
+  });
+
+  $('button').on('click', function() {
     sunrise();
   });
 
@@ -20,25 +17,25 @@ $(document).ready(function() {
     city: null
   };
 
-function sunrise() {
-  $.ajax({
-   url : "http://api.wunderground.com/api/"+user.apiKey+"/astronomy/q/"+user.state+"/"+user.city+".json",
-   dataType : "jsonp",
-   success : function(data) {
-    var source = $('#template').html();
-    var template = Handlebars.compile(source);
-    var context = {
+  function sunrise() {
+    $.ajax({
+      url: "http://api.wunderground.com/api/"+user.apiKey+"/astronomy/q/"+user.state+"/"+user.city+".json",
+      dataType: "jsonp",
+      success: function(data) {
+        var source = $('#template').html();
+        var template = Handlebars.compile(source);
+        var context = {
       state: user.state,
       city: user.city,
       sunrise: data.sun_phase.sunrise.hour + ":" + data.sun_phase.sunrise.minute ,
       sunset: data.sun_phase.sunset.hour + ":" + data.sun_phase.sunset.minute
-    };
-    var html = template(context);
-    $('.content-container').prepend(html).fadeIn('slow');
-    console.log(data.sun_phase.sunrise);
-    console.log(data.sun_phase.sunset);
-    console.log(data);
-   }
- });
- }
+      };
+      var html = template(context);
+      $('.content-container').prepend(html).fadeIn('slow');
+      console.log(data.sun_phase.sunrise);
+      console.log(data.sun_phase.sunset);
+      console.log(data);
+      }
+    });
+  }
 });
